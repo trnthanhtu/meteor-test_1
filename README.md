c53f041121bacf97f4827ce4870e72c360ec8aae
trnthanhtu <tranthanhtu10t1bkdn@gmail.com>
2016-02-28 22:59:24 +0700

    Finished Session core

---
+++ README.md
@@ -0,0 +1,13 @@
+# meteor-test_1
+Test meteor with simple application.
+
+**Terminal command**
+
+`meteor create [name]` 
+
+run app
+
+`meteor run`
+
+
+
--- leaderboard.js
+++ leaderboard.js
@@ -5,13 +5,21 @@ if (Meteor.isClient) {
   Template.listplayers.helpers({
     'list' : function(){
       return Playerlists.find()
+    },
+    'selectedClass' : function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+
+      if (playerId == selectedPlayer) {
+        return 'selected'
+      }
     }
   })
 
   Template.listplayers.events({
     'click .player': function(){
-      //Add event function
-      console.log('You clicked something');
+      var playerId = this._id;
+      var selectedPlayer = Session.set('selectedPlayer', playerId);
     }
   })
 }
