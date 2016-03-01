944f7fee008bedf832a6063d301f561722e05fbb
at-tutt <tutt@asiantech.vn>
2016-03-01 17:40:43 +0700

    Interact with form

--- leaderboard.js
+++ leaderboard.js
@@ -4,7 +4,7 @@ Playerlists  = new Mongo.Collection('list');
 if (Meteor.isClient) {
   Template.listplayers.helpers({
     'list' : function(){
-      return Playerlists.find()
+      return Playerlists.find({}, {sort: {score: -1}})
     },
     'selectedClass' : function(){
       var playerId = this._id;
@@ -12,6 +12,14 @@ if (Meteor.isClient) {
 
       if (playerId == selectedPlayer) {
         return 'selected'
+      }
+    },
+    'disable': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+
+      if (playerId == selectedPlayer) {
+        return 'false'
       }
     }
   })
@@ -25,9 +33,33 @@ if (Meteor.isClient) {
       var selectedPlayer = Session.get('selectedPlayer');
 
       // increase score +10
-      Playerlists.update(selectedPlayer, {$inc: {score: 10} });
+      Playerlists.update(selectedPlayer, {$inc: {score: 10}});
+    },
+    'click .decrement': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      Playerlists.update(selectedPlayer, {$inc: {score: -10}});
+    },
+    'click .remove': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      Playerlists.remove(selectedPlayer);
     }
-  })
+  });
+
+  // Add Template for submit event
+  Template.addPlayerForm.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var params = {
+        name: $('#playerName').val(),
+        score: parseInt($('#score').val())
+      };
+
+      // Insert to database
+
+      Playerlists.insert(params);
+    }
+
+  });
 }
 
 if (Meteor.isServer) {
